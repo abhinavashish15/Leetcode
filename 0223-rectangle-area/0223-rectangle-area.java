abb28cfe6d167a16abaa8ca93604a608a19175e3
Abhinav Ashish <100207127+abhinavashish15@users.noreply.github.com>
2024-01-27 18:51:00 +0530

    Time: 2 ms (44.87%), Space: 42.2 MB (63.14%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1=(ax2-ax1)*(ay2-ay1);
+      int area2=(bx2-bx1)*(by2-by1);
+      
+      int xc=Math.min(ax2,bx2)-Math.max(ax1,bx1);
+      int yc=Math.min(ay2,by2)-Math.max(ay1,by1);
+      
+      int result=0;
+      if(xc>0&&yc>0){
+        result=xc*yc;
+      }
+      int total=area1+area2-result;
+      return total;
+    }
+  // private static void intersectArea()
+}
