6e7cc26b63ba56664dc4281412e1d6b09fa93dab
Abhinav Ashish <100207127+abhinavashish15@users.noreply.github.com>
2024-01-29 23:56:46 +0530

    Update 0912-sort-an-array.java

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,50 @@
+# code using built-in function:
+
 class Solution {
     public int[] sortArray(int[] nums) {
       Arrays.sort(nums);
         return nums;
     }
 }
+
+#code using quickSort:
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+      // Arrays.sort(nums);
+      //   return nums;
+      quickSort(nums,0,nums.length-1);
+      return nums;
+    }
+  private static void quickSort(int nums[], int low,int high){
+    if(low<high){
+      int part=partition(nums,low, high);
+      
+      quickSort(nums,low,part-1);
+      quickSort(nums,part+1,high);
+    }
+  }
+    private static int partition(int nums[],int start,int end){
+      int pivot=nums[end];
+      int x=start-1;
+      for(int i=start;i<=end;i++){
+        if(nums[i]<pivot){
+          x++;
+          swap(nums,x,i);
+          // int temp=nums[x];
+          // nums[x]=nums[i];
+          // nums[i]=temp;
+        }
+      }
+      swap(nums,x+1,end);
+      // int t=nums[x+1];
+      // nums[x+1]=nums[end];
+      // nums[end]=t;
+      return x+1;
+    }
+    public static void swap(int nums[],int a, int b){
+      int temp=nums[a];
+      nums[a]=nums[b];
+      nums[b]=temp;
+    }
+  }
