a0aab3ef6ba35d67f7b70a9ce8882b571083e48c
Abhinav Ashish <100207127+abhinavashish15@users.noreply.github.com>
2024-02-05 23:34:02 +0530

    Update 0223-rectangle-area.java

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -2,8 +2,9 @@ class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
         int area1=(ax2-ax1)*(ay2-ay1);
       int area2=(bx2-bx1)*(by2-by1);
-      
+      // x-coordinate
       int xc=Math.min(ax2,bx2)-Math.max(ax1,bx1);
+        // y coordinate
       int yc=Math.min(ay2,by2)-Math.max(ay1,by1);
       
       int result=0;
