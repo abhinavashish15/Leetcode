f66e75dde13389b839ede686f4e11baa17dd3855
Abhinav Ashish <100207127+abhinavashish15@users.noreply.github.com>
2024-02-06 23:34:36 +0530

    Update 0042-trapping-rain-water.java

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -4,6 +4,7 @@ class Solution {
 
         int leftMax[] = new int[n];
         leftMax[0] = height[0];
+        // finding left heighest peak
         for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
